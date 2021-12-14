1201a69bd8ee7aad11256327d495aa4191122345
tinou_99 <tinou99.f@gmail.com>
2021-12-15 00:37:23 +0100

    chg: button to show solution on same generated grid added

---
+++ api/static/show_solution.js
@@ -0,0 +1,11 @@
+function showSolution(grid_solved){
+    let gridHTML = '';
+    for (let row of grid_solved){
+        gridHTML += '<tr>';
+        for (let col of row){
+            gridHTML += '<td>'+ col +'</td>';
+        }
+        gridHTML += '</tr>';
+    }
+    document.getElementById('sudoku_grid').innerHTML = gridHTML;
+}
