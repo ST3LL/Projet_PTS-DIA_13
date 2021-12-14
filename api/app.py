59ecf3c1af4fa06011b90bd4231840fb9125006d
tinou_99 <tinou99.f@gmail.com>
2021-12-14 01:30:43 +0100

    chg: api modified

--- api/app.py
+++ api/app.py
@@ -1,9 +1,16 @@
 from flask import Flask, render_template, request
-from sudoku import Game, build_vanilla_region_map
+from sudoku import build_vanilla_region_map
 from models import *
+from flask_wtf import FlaskForm
+from wtforms import SelectField
 
 
 app = Flask(__name__)
+
+
+class Form(FlaskForm):
+    models = SelectField('models', choices=get_models_and_rules().keys())
+    rules_from_models = SelectField('rules', choices=[])
 
 
 @app.errorhandler(404)
@@ -13,31 +20,36 @@ def page_not_found(e):
 
 @app.route('/', methods=['GET', 'POST'])
 def home_app():
-    return render_template('home.html', models=get_models_rules())
+    form = Form()
+    form.rules_from_models.choices = [get_models_and_rules()[m] for m in get_models_and_rules().keys()]
+    if request.method == 'POST':
+        return
+
+    return render_template('home.html', form=form, models=get_models_and_rules())
 
 
 @app.route('/generate_solution/', methods=['GET', 'POST'])
 def GenerateSolution():
     if request.method == 'POST':
-        models_selected = request.form.get('comp')
+        models_selected = request.form.get('comp_select1')
         print(models_selected)
-        l_rules_from_model = get_rules_of_model(models_selected)
         get_dim_generation = request.form.get('dim_sudoku_generation')
-        game = build_sudoku('vanilla', build_vanilla_region_map(dim=int(get_dim_generation)), l_rules_from_model)
-        # grid = game.solution
-        return render_template('generate_solution.html',
-                               grid=grid)
+        print(get_dim_generation)
+        sudoku = build_sudoku(models_selected, build_vanilla_region_map(dim=int(get_dim_generation)), ['rule_row', 'rule_col', 'rule_region'])
+        sol = sudoku.solution
+        return render_template('generate_solution.html', grid=sol)
 
 
 @app.route("/new_sudoku/", methods=['GET', 'POST'])
 def NewSudoku():
     if request.method == 'POST':
         chars = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
-        get_dim = request.form.get('dim_sudoku')
-        game = Game(build_vanilla_region_map(dim=int(get_dim)))
-        grid_solved = game.solution
-        grid = game.grid
-        print(grid_solved)
+        models_selected = request.form.get('comp_select2')
+        get_dim_generation = request.form.get('dim_sudoku')
+        sudoku = build_sudoku(models_selected, build_vanilla_region_map(dim=int(get_dim_generation)), ['rule_row', 'rule_col', 'rule_region'])
+        grid_solved = sudoku.solution
+        grid = sudoku.grid
+        print(grid)
         return render_template("new_sudoku.html",
                                grid_solved=grid_solved,
                                grid=grid,
