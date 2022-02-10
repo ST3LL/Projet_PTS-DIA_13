2f076874e6e9a6f12f564632ea66a5f5b4ab4fb1
tinou_99 <tinou99.f@gmail.com>
2022-02-10 12:21:47 +0100

    chg: checkboxes for rules selection implemented

--- api/static/rule_selector.js
+++ api/static/rule_selector.js
@@ -2,7 +2,8 @@ function updateRulesSelector(model, d_models){
     l_rules = d_models[model];
     let optionHTML = '';
     for (let r of l_rules){
-        optionHTML += '<option value="' + r + '">' + r + '</option>';
+        /* optionHTML += '<option value="' + r + '">' + r + '</option>'; */
+         optionHTML += '<input type="checkbox" id="comp_select2" name="comp_select2" value=value="' + r + '">' + r + '>';
     }
     document.getElementById('comp_select2').innerHTML = optionHTML;
 }
