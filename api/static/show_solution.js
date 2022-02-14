6b730d7986d42843f31d25754445805215da7e24
Kevin Gomes <kevingomeskevin@gmail.com>
2022-02-14 19:16:21 +0100

    chg : color for the solution, remove border

--- api/static/show_solution.js
+++ api/static/show_solution.js
@@ -1,9 +1,9 @@
-function showSolution(grid_solved){
+function showSolution(grid_solved, colors, region_map){
     let gridHTML = '';
-    for (let row of grid_solved){
+    for (let row = 0; row < grid_solved.length; row++){
         gridHTML += '<tr>';
-        for (let col of row){
-            gridHTML += '<td>'+ col +'</td>';
+        for (let col = 0; col < grid_solved[row].length; col++){
+            gridHTML += '<td style="background-color: ' + colors[region_map[row][col]] + '50;">'+ grid_solved[row][col] +'</td>';
         }
         gridHTML += '</tr>';
     }
